8a95943005cf4e3dd378392039e22f5f25a38fe8
DIVYESH-S0NI <129260295+DIVYESH-S0NI@users.noreply.github.com>
2023-07-10 21:17:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// for (let i = 1; i <= 5; i++) {
+//   document.write(i);
+// }
+function call(NEW, LAST) {
+  console.log('hello' + " " + NEW + " " + LAST);
+
+};
+call('GS', 'KS');
+
+let r = 10;
+let p = 20;
+let q = "The result is: " + r + p;
+console.log(q);
+
+function add(num1, num2) {
+  let aa = num1 + num2;
+  console.log(aa);
+}
+add(10, 10);
+
+
+
+
+
